1b77db9247fa1919941cef31eeb8e8affa59a972
Alex <alex@susanu.com>
2023-02-15 18:55:55 +0000

    added chi

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome</h1>")
 }
@@ -20,7 +21,7 @@ type Router struct{}
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
-		homeHandler(w)
+		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
@@ -30,7 +31,14 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var router Router
-	fmt.Println("Starting on :3000")
-	http.ListenAndServe(":3000", router)
+	//var router Router
+	//fmt.Println("Starting on :3000")
+	//http.ListenAndServe(":3000", router)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	http.ListenAndServe(":3000", r)
 }
